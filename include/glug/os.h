db12f9679fe12a9e509862ecf19f70cf83fd5fc2
Dustin Cook <dustin@libglug.org>
2019-11-10 17:25:20 -0800

    define current GLUG_OS as GLUG_OS_XXX

--- include/glug/os.h
+++ include/glug/os.h
@@ -1,27 +1,37 @@
+#define GLUG_OS_UNK 0
+#define GLUG_OS_WIN 1
+#define GLUG_OS_MAC 2
+#define GLUG_OS_LIN 3
+#define GLUG_OS_BSD 4
+
 #if defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
 
-    #define GLUG_OS_WIN
+    #define GLUG_OS GLUG_OS_WIN
 
 #elif defined(__APPLE__) && defined(__MACH__)
 
     #include <TargetConditionals.h>
     #if TARGET_OS_MAC == 1
 
-        #define GLUG_OS_MAC
+        #define GLUG_OS GLUG_OS_MAC
 
     #endif // TARGET_OS_MAC
 
 #elif defined(__linux__)
 
-    #define GLUG_OS_LIN
+    #define GLUG_OS GLUG_OS_LIN
 
 #elif defined(__unix__)
 
     #include <sys/param.h>
     #if defined(BSD)
 
-        #define GLUG_OS_BSD
+        #define GLUG_OS GLUG_OS_BSD
 
     #endif // BSD
+
+#else
+
+    #define GLUG_OS GLUG_OS_UNK
 
 #endif
