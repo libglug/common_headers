d7ab6d71988056793cc472796bf55f284f80753a
Dustin Cook <dustin@libglug.org>
2020-07-05 23:20:21 -0700

    add reallocator

--- include/glug/allocator_t.h
+++ include/glug/allocator_t.h
@@ -5,6 +5,7 @@
 
 typedef void *(*glug_malloc_t) (size_t nbytes);
 typedef void  (*glug_free_t)   (void *block);
+typedef void *(*glug_realloc_t)(void *block);
 
 struct glug_allocator
 {
@@ -12,8 +13,15 @@ struct glug_allocator
     glug_free_t    free;
 };
 
+struct glug_reallocator
+{
+    struct glug_allocator allocator;
+    glug_realloc_t        realloc;
+};
+
 #ifdef GLUG_USE_TYPEDEFS
-    typedef struct glug_allocator glug_allocator_t;
+    typedef struct glug_allocator   glug_allocator_t;
+    typedef struct glug_reallocator glug_reallocator_t;
 #endif
 
 #endif // GLUG_ALLOCATOR_H
